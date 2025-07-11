9d11e3ef88c9dd966aa4073167a8585f28eb46fb
Shreckhise <shrecjx@UIRLA-WMX6YD9.abbvienet.com>
2025-07-11 10:12:43 -0400

    update test

--- apps/react-app-1/src/app/app.tsx
+++ apps/react-app-1/src/app/app.tsx
@@ -3,7 +3,8 @@ import { isLoadingRuntimeVar } from '../application.config';
 export function App() {
   return (
     <div>
-      <p>Build time var: {import.meta.env.VITE_TEST_VAR}</p>
+      <h1>Welcome react-app-1</h1>
+      <p>Build time var: {process.env.VITE_TEST_VAR}</p>
       <p>Runtime var: {window.__RUNTIME_CONFIG__?.TEST_RUNTIME_VAR}</p>
       <p>is loading runtime env: {isLoadingRuntimeVar}</p>
     </div>
