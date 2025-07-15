56cb4b080aa1a408b3f15b6904ce61063afb0fe8
Shreckhise <shrecjx@UIRLA-WMX6YD9.abbvienet.com>
2025-07-15 16:38:09 -0400

    add ts configs package

--- apps/react-app-1/src/main.tsx
+++ apps/react-app-1/src/main.tsx
@@ -1,29 +1,44 @@
-// import { StrictMode } from 'react';
-// import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-import { isHostedConfig } from './types/hostedConfig';
+import { isHostedConfig } from '@libs/configs/types/hostedConfig';
+import localConfig from '@libs/configs/local/react-app-1.json';
 
 const loadRuntimeConfig = async () => {
-  await fetch('https://d2utt9g60l5nz9.cloudfront.net/prod.json')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((config) => {
-      window.__RUNTIME_CONFIG__ = config;
-      if (!isHostedConfig(config)) {
-        console.warn('Invalid runtime config format');
-      }
-    })
-    .catch((error) => {
-      console.error('Failed to load runtime config', error);
-    });
+  if (window.location.hostname === 'localhost') {
+    window.__RUNTIME_CONFIG__ = localConfig;
+    if (!isHostedConfig(localConfig)) {
+      console.warn('Invalid runtime config format');
+    }
+  } else {
+    await fetch('https://d2utt9g60l5nz9.cloudfront.net/prod.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((config) => {
+        window.__RUNTIME_CONFIG__ = config;
+        if (!isHostedConfig(config)) {
+          console.warn('Invalid runtime config format');
+        }
+      })
+      .catch((error) => {
+        console.error('Failed to load runtime config', error);
+      });
+  }
 };
 
 loadRuntimeConfig().then(() => {
+  if (window.location.hostname === 'localhost') {
+    const link =
+      (document.querySelector("link[rel~='icon']") as HTMLLinkElement) ||
+      (document.createElement('link') as HTMLLinkElement);
+    link.rel = 'icon';
+    link.href = '/favicon-local.ico';
+    document.head.appendChild(link);
+  }
+
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   );
